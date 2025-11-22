82f339bffa3aeff87a0ca96664f0bae39135aacb
Damian Garcia <damiangarcia@Damians-MacBook-Air.local>
2025-11-21 20:20:32 -0600

    feat(add): Generate a resource with command - nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [ProjectsModule, AuthModule, TasksModule, UsersModule],
+  imports: [ProjectsModule, AuthModule, TasksModule, UsersModule, PaymentsModule],
   controllers: [HelloController]
 })
 export class AppModule {}
