2af3168e5a153208c1cff42600187b4869118cfe
Damian Garcia <damiangarcia@Damians-MacBook-Air.local>
2025-11-21 19:14:35 -0600

    feat(add): http status

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res, HttpCode } from '@nestjs/common';
 import type { Request, Response } from 'express';
 
 @Controller('hello')
@@ -8,5 +8,23 @@ export class HelloController {
     getHello(@Req() req: Request, @Res() res: Response) {
         console.log(req.url);
         return res.status(200).send({ message: 'Hello, World!' });
+    }
+
+    @Get('/new')
+    @HttpCode(201)
+    newHello() {
+        return { message: 'Hello from the new route!' };
+    }
+
+    @Get('/not-found')
+    @HttpCode(404)
+    notFountPage() {
+        return { message: 'Resource not found' };
+    }
+
+    @Get('/error')
+    @HttpCode(500)
+    errorPage() {
+        return { message: 'Internal server error' };
     }
 }
